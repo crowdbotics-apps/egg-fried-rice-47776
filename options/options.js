b39930921fc27311d2d44cfc61f7ab732c574a39
Crowdbotics <team@crowdbotics.com>
2024-05-02 08:03:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "egg-fried-rice-47776",
+  url: "https://egg-fried-rice-47776.botics.co",
+  api: "https://egg-fried-rice-47776.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
